56471d80bffcb45ae7adbb0750fa6fc1bbe97893
Iwan <izzikhwan1408@gmail.com>
2026-06-04 12:57:17 +0800

    Test awal

---
+++ src/main/java/com/csc3402/smartlibrarysystem/model/Book.java
@@ -0,0 +1,5 @@
+package com.csc3402.smartlibrarysystem.model;
+
+public class Book {
+
+}
---
+++ src/test/java/com/csc3402/smartlibrarysystem/SmartLibrarySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csc3402.smartlibrarysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartLibrarySystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
